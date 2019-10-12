b523970bb6d52b60d812df3436a5a591c92e95b7
shin <shin.ibrahim1226@gmail.com>
2019-10-12 19:48:35 +0900

    default

--- app/src/main/java/com/example/devapp/MainActivity.kt
+++ app/src/main/java/com/example/devapp/MainActivity.kt
@@ -2,51 +2,11 @@ package com.example.devapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import com.google.firebase.firestore.DocumentReference
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.SetOptions
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var db: DocumentReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        db = FirebaseFirestore.getInstance().document("NowIsHot/Member")
-
-        val button = findViewById<Button>(R.id.save)
-
-        button.setOnClickListener {
-            val inputKeyEditText = findViewById<EditText>(R.id.inputKey)
-            val inputValueEditText = findViewById<EditText>(R.id.inputValue)
-            val inputKeyText = inputKeyEditText.text.toString()
-            val inputValueText = inputValueEditText.text.toString()
-            var isFailed = false
-            if (inputKeyText.isEmpty()) {
-                inputKeyEditText.error = "Please input anything."
-                isFailed = true
-            }
-            if (inputValueText.isEmpty()) {
-                inputValueEditText.error = "Please input anything."
-                isFailed = true
-            }
-
-            if (isFailed) {
-                return@setOnClickListener
-            }
-            val dataToSave = hashMapOf(
-                inputKeyText to inputValueText
-            )
-
-            db
-                .set(dataToSave, SetOptions.merge())
-                .addOnSuccessListener { Log.d("print", "Document successfully written!") }
-                .addOnFailureListener { e -> Log.w("print", "Error writing document", e) }
-        }
     }
 }
