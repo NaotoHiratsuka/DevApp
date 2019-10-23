6d87559dadf268955edf955449ea66e9d9f7037c
Naoto <m143350nn@gmail.com>
2019-10-23 23:50:38 +0900

    Added menu.

--- app/src/main/java/com/example/devapp/Chat.kt
+++ app/src/main/java/com/example/devapp/Chat.kt
@@ -1,14 +1,13 @@
 package com.example.devapp
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
+import android.view.*
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -19,6 +18,21 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class ChatActivity : AppCompatActivity() {
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId)  {
+            R.id.top -> {
+                val intent = Intent(this, TitleActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,8 +61,8 @@ class ChatActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(
             this, LinearLayoutManager.VERTICAL, false)
 
-        val button = findViewById<Button>(R.id.send)
-        button.setOnClickListener {
+        val arrow = findViewById<ImageView>(R.id.send)
+        arrow.setOnClickListener {
             val inputKeyEditText = findViewById<EditText>(R.id.inputKey)
             val inputKeyText = inputKeyEditText.text.toString()
 
